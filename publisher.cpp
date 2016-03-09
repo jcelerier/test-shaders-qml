4e6231759210ed7f4e26459ca0a391330255c73a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-03-10 00:29:48 +0100

    update

--- publisher.cpp
+++ publisher.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 Publisher::Publisher(int pPort, int rPort) : Transmitter(pPort, rPort),
-    m_dev(m_base_map, "newDevice", 9999, "127.0.0.1", 6666)
+    m_dev(m_base_map, "newDevice", 9998, "127.0.0.1", 13579)
 {
 
 }
